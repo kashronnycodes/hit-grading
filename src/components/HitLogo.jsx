50915c8d1e176c656c06cc38cfebfe480b3cfbc8
kashronny <ronron.manago1000@gmail.com>
2026-05-19 02:53:47 +0800

    Add HIT logo component

---
+++ src/components/HitLogo.jsx
@@ -0,0 +1,8 @@
+export function HitLogo() {
+  return (
+    <div className="hit-logo" aria-label="HIT Grading Redefined">
+      <div className="hit-mark">HIT</div>
+      <div className="hit-subtitle">GRADING REDEFINED</div>
+    </div>
+  );
+}
